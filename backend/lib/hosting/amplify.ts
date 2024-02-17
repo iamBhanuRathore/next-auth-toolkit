af3544c43677aa3490b4d5dfc2ec49e01eeeee28
bhanuo07 <rathoreb853@gmail.com>
2024-02-17 12:14:33 +0530

    .

--- backend/lib/hosting/amplify.ts
+++ backend/lib/hosting/amplify.ts
@@ -1,6 +1,7 @@
-import * as amplify from "@aws-cdk/aws-amplify-alpha";
-import { SecretValue } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { SecretValue } from "aws-cdk-lib";
+import * as amplify from "@aws-cdk/aws-amplify-alpha";
+import { BuildSpec } from "aws-cdk-lib/aws-codebuild";
 type Props = {
   appName: string;
   branch: string;
@@ -20,6 +21,7 @@ export function createAmplifyHosting(
       repository: repo,
       oauthToken: SecretValue.secretsManager(ghTokenName),
     }),
+    platform: amplify.Platform.WEB_COMPUTE,
     autoBranchDeletion: true,
     customRules: [
       {
@@ -31,6 +33,26 @@ export function createAmplifyHosting(
     environmentVariables: {
       myAmplifyEnv: "fontend", //process.env.myAmplifyEnv
     },
+    buildSpec: BuildSpec.fromObjectToYaml({
+      version: 1,
+      frontend: {
+        phases: {
+          prebuild: {
+            commands: ["npm i"],
+          },
+          build: {
+            commands: ["npm run build"],
+          },
+        },
+        artifacts: {
+          baseDirectory: ".next",
+          files: ["**/*"],
+        },
+        cache: {
+          paths: ["node_modules/**/*"],
+        },
+      },
+    }),
   });
   amplifyApp.addBranch(branch, {
     stage: branch === "main" ? "PRODUCTION" : "DEVELOPMENT",
